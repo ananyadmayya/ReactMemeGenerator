572f68470f893620a89fb2c0b955fc6114f39bca
ananyadmayya <ananya.19cs009@sode-edu.in>
2023-12-13 22:39:51 +0530

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from './components/header'
+import Meme from './components/Meme'
+
+export default function() {
+  return (
+    <div>
+    <Header />
+    <Meme />
+    </div>
+  )
+}
